ea2a819aaccf7296549a4a7e190cea160cbd9f98
MrB9751 <162899668+MrB9751@users.noreply.github.com>
2025-12-10 13:37:09 -0500

    more cleanups in Shooter subsystem

--- src/main/java/frc/robot/subsystem/Shooter.java
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -5,31 +5,29 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    private final SparkMax m_intakemotor;
-    public double distance;
+    // This declares a motor controller for the shooter
+    private final SparkMax m_leadmotor;
 
     private Shooter () {
-        m_intakemotor = new SparkMax (1, MotorType.kBrushless);
-        // Replace 1 with the actual port number of your motor 
+        // This initializes the lead motor controller for the shooter
+        m_leadmotor = new SparkMax (1, MotorType.kBrushless);  // Replace 1 with the actual port number of your motor 
     } 
 
-    public double getDistance() {
-        return distance;
-    }
-    
+  //  This is the Singleton design pattern: It creates the single instance of the Shooter class to be used by other classes
     public static class instanceHolder { 
     private static final Shooter INSTANCE = new Shooter();
     }
 
+    // This is the "getter" method that other classes use to obtain the single instance of the Shooter class
     public static Shooter getInstance() {
     return instanceHolder.INSTANCE;
     }
 
     public void setSpeed (double speed) {
-    m_intakemotor.set(speed);
+    m_leadmotor.set(speed);
     }
     
     public void stop() {
-        m_intakemotor.set(0);
+        m_leadmotor.set(0);
     }
 }
